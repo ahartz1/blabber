e5f7697678a98874a08ada82b269d79e329b3ba8
Adam Hartz <ahartz1@gmail.com>
2015-10-06 12:17:52 -0400

    Add updates app

---
+++ blabber/updates/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Status(models.Model):
+    # Note parentheses after; instances of classes
+    # id is automatic
+    text = models.CharField(max_length=141)
+    posted_at = models.DateTimeField()
+    user = models.ForeignKey(User)
+    # this is how the relations are made within db
